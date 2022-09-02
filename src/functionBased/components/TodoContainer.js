e3afa180292f1e7e1ca3cabfa92789ff317b7b9c
warui09 <93465580+warui09@users.noreply.github.com>
2022-09-02 19:07:50 +0300

    edited import statement in TodoContainer.js

--- src/functionBased/components/TodoContainer.js
+++ src/functionBased/components/TodoContainer.js
@@ -1,5 +1,5 @@
 import React from "react";
-import TodosList from "./TodosList";
+import TodosList from "./TodoList";
 import Header from "./Header";
 import InputTodo from "./InputTodo";
 import { v4 as uuidv4 } from "uuid";
