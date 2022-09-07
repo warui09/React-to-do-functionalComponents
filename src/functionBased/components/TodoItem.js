49e58aa267a238573319dd22218d10583bd192f4
warui09 <93465580+warui09@users.noreply.github.com>
2022-09-07 10:11:27 +0300

    added  useEffect to clear on component unmount

--- src/functionBased/components/TodoItem.js
+++ src/functionBased/components/TodoItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../TodoItem.module.css";
 
 const TodoItem = (props) => {
@@ -21,7 +21,7 @@ const TodoItem = (props) => {
     textDecoration: "line-through"
   };
 
-  const { completed, id, title } = this.props.todo;
+  const { completed, id, title } = props.todo;
 
   let viewMode = {};
   let editMode = {};
@@ -31,6 +31,12 @@ const TodoItem = (props) => {
   } else {
     editMode.display = "none";
   }
+
+  useEffect(() => {
+    return () => {
+      console.log("cleaning up...");
+    };
+  }, []);
 
   return (
     <li key={id} className={styles.item}>
